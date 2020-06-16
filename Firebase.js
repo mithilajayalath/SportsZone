692341be2d59788df5f6a9df7f125e60cc6a014b
RusJ <rjillesingha@gmail.com>
2020-06-16 14:39:49 +0530

    insertion to db

---
+++ Firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+    apiKey: "AIzaSyCTLmBkMtK8-w0Opk15JYIE8iEVYIsgY7k",
+    authDomain: "sportszone-fd46a.firebaseapp.com",
+    databaseURL: "https://sportszone-fd46a.firebaseio.com",
+    projectId: "sportszone-fd46a",
+    storageBucket: "sportszone-fd46a.appspot.com",
+    messagingSenderId: "202671100771",
+    appId: "1:202671100771:web:1940a29e0770a1321d7956",
+    measurementId: "G-KKVTKLWQFE"
+    };
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
